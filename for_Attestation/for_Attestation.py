52580d47611160e6a9e653c7bed3643c0aeaa215
Korolev Ivan <Ivankorol1512@yandex.ru>
2025-07-27 22:25:23 +0300

    empty

---
+++ for_Attestation/for_Attestation.py
@@ -0,0 +1,11 @@
+﻿
+import pandas as pd
+import random
+import sys
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+
+df = pd.read_excel('D:/other_Projects/for_Attestation/TestData.xlsx')
+print(df)
